6b66c893f355ee63621bdf1b1989d8f71cce6a00
ff5h <vdj.manka@gmail.com>
2022-05-21 01:57:32 +0300

    Add project files.

---
+++ Adult.App/Adult.App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Adult.App.Models;
+using Adult.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Adult.App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
